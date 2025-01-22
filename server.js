ba0e2b37801ed3b4f30cbf5a58f57fa1e14687ae
byronoko <byron.ux@gmail.com>
2025-01-22 19:28:22 +0000

    Convert server.js to ES module syntax

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
+
+// Get the directory name of the current module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Serve the React build files from the "dist" directory
 app.use(express.static(path.join(__dirname, 'dist')));
