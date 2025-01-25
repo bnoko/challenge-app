23f4cbf4b9ebf198f68df74561d78dccb881cf84
byronoko <byron.ux@gmail.com>
2025-01-25 14:42:00 +0000

    Remove unused 'Navigate' import to fix TypeScript error

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import PrivacyPolicy from "./pages/PrivacyPolicy"; // Import the new Privacy Policy component
 import TermsOfService from "./pages/TermsOfService"; // Import the new Terms of Service component
 import SignIn from "./pages/SignIn"; // Adjust the path to your SignIn.tsx file
